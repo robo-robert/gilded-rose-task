2247b409463549365399921fcf1540630bcfe02a
Robertas Skalskas <robertas@novameta.lt>
2021-02-17 21:31:20 +0200

    Initial testing of Mr. Leeroy's code
    commit 4db1a9f7b8fe11bd4384c495aa2d22cf61403bc9
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 21:31:07 2021 +0200
    
        Added some comments after finishing testing initial code
    
    commit fa347c516652ac894be7e39d5a1140ac8a5b2771
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 21:28:49 2021 +0200
    
        Removed test with multiple random items in order to reduce test file
    
    commit a0bed9712e865381f633929ad7df1b52723d4911
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 21:27:15 2021 +0200
    
        Added missing line in Sulfuras, Hand of Ragnaros
    
    commit 583d2eec203a85c825712a0362dad3db2ba5616e
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 21:26:53 2021 +0200
    
        Added test cases for Backstage passes to a TAFKAL80ETC concert
    
    commit cde9d9e46b2758ba91c1ac294f1fbfd5c578af85
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 20:56:50 2021 +0200
    
        Added some test cases for Sulfuras, Hand of Ragnaros
    
    commit 7badee22d990d6d6a301a634619dbaeaa43311eb
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 20:46:51 2021 +0200
    
        Added some more test for Aged Brie
    
    commit 585fcafb37004b806c697585559c3d934501b5c2
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 20:33:10 2021 +0200
    
        Fixed typo in the comment
    
    commit c753cb5675d1cd501163e9a37217605289310785
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 20:32:39 2021 +0200
    
        Added some test for multiple items for safety
    
    commit 7c196e70fac40026d168a8341ef61f04e39a8d42
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 20:28:01 2021 +0200
    
        Added sellIn reduce test for multiple random items
    
    commit 9a5eaab90efd69053a8e7f279b77d637f739dc9c
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 20:22:24 2021 +0200
    
        Added two more test cases for single random item
    
    commit 7ad613fda96a70ae2a486638544e37e30824d92a
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 20:17:40 2021 +0200
    
        Added quality reduction test for single random item when sellIn < 0
    
    commit 2dc6fbda68717c4b6f1d1e1ba05e623fa21689ef
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 20:14:36 2021 +0200
    
        Added quality reduction test for single random item
    
    commit 1fe46304b31d702c04e4e6bbca764407ece76d1b
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 20:12:11 2021 +0200
    
        Added sellIn reduction test for single random item
    
    commit 361ecd095032b64f8c45cd556568159cf89c9d1d
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 20:08:49 2021 +0200
    
        Started to implement test cases with a single item
    
    commit 93907a17a78d3f75b19c0e5c08c7baf41b84ab86
    Author: Robertas Skalskas <robertas@novameta.lt>
    Date:   Wed Feb 17 20:04:28 2021 +0200
    
        Wrote some thoughts about testing

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -1,8 +1,22 @@
 import { expect } from 'chai';
 
-import { GildedRose } from '../src/gilded-rose';
+import { GildedRose, Item } from '../src/gilded-rose';
 
+/*
+    Intially I will separate test cases
+    -- With single random item
+    -- With single Aged Brie
+    -- With single Sulfuras, Hand of Ragnaros
+    -- With single Backstage passes to a TAFKAL80ETC concert
+
+    Finished initial testing
+    -- % Stms 100%
+    -- % Branch 100%
+    -- % Funcs 100%
+    -- % Lines 100%
+*/
 describe(`Gilded Rose package`, () => {
+
     describe(`It works`, () => {
         let gildedRose: GildedRose;
 
@@ -12,6 +26,144 @@ describe(`Gilded Rose package`, () => {
 
         it(`Should create an empty instance of GuildedRose`, () => {
             expect(gildedRose.items.length).to.equal(0);
+        });
+    });
+
+    describe(`With single random item`, () => {
+        let gildedRose: GildedRose;
+
+        beforeEach(() => {
+            gildedRose = new GildedRose([new Item('Random item 1', 10, 15)]);
+        });
+
+        it(`Should add Random item 1 to to the GildedRose instance`, () => {
+            expect(gildedRose.items[0].name).to.equal('Random item 1');
+        });
+
+        it(`Should reduce sellIn from 10 to 9 on updateQuality`, () => {
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].sellIn).to.equal(9);
+        });
+
+        it(`Should reduce quality from 15 to 14 on updateQuality`, () => {
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(14);
+        });
+
+        it(`When sellIn < 0 should reduce quality from 15 to 13 on updateQuality`, () => {
+            gildedRose.items[0].sellIn = -1;
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(13);
+        });
+
+        it(`When sellIn < 0 and quality is 1 should reduce quality from 1 to 0 on updateQuality`, () => {
+            gildedRose.items[0].sellIn = -1;
+            gildedRose.items[0].quality = 1;
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(0);
+        });
+
+        it(`When quality is 0 should not reduce quality on updateQuality`, () => {
+            gildedRose.items[0].quality = 0;
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(0);
+        });
+
+        /* I will be back here in the future */
+    });
+
+    describe(`With single Aged Brie`, () => {
+        let gildedRose: GildedRose;
+
+        beforeEach(() => {
+            gildedRose = new GildedRose([new Item('Aged Brie', 5, 10)]);
+        });
+
+        it(`Should increase quality from 10 to 11 on updateQuality`, () => {
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(11);
+        });
+
+        it(`When quality is 50 should not increase quality on updateQuality`, () => {
+            gildedRose.items[0].quality = 50;
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(50);
+        });
+
+        it(`When sellIn < 0 should increase quality from 10 to 12 on updateQuality`, () => {
+            gildedRose.items[0].sellIn = -1;
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(12);
+        });
+
+        it(`When sellIn < 0 should increase quality from 49 to 50 on updateQuality`, () => {
+            gildedRose.items[0].sellIn = -1;
+            gildedRose.items[0].quality = 49;
+            const actualUpdatedItem = gildedRose.updateQuality()[0];
+            expect(actualUpdatedItem.quality).to.equal(50);
+        });
+    });
+
+    describe(`With single Sulfuras, Hand of Ragnaros`, () => {
+        let gildedRose: GildedRose;
+
+        beforeEach(() => {
+            gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 20, 30)]);
+        });
+
+        it(`Should not alter quality on updateQuality`, () => {
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(30);
+        });
+
+        it(`Should not alter sellIn on updateQuality`, () => {
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].sellIn).to.equal(20);
+        });
+
+        it('When sellIn < 0 should not decrease quality on updateQuality', () => {
+            gildedRose.items[0].sellIn = -1;
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(30);
+        });
+    });
+
+    describe(`With single Backstage passes to a TAFKAL80ETC concert`, () => {
+        let gildedRose: GildedRose;
+
+        beforeEach(() => {
+            gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)]);
+        });
+
+        it(`When sellIn > 11 should increase quality to 21 on updateQuality`, () => {
+            gildedRose.items[0].sellIn = 12;
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(21);
+        });
+
+        it(`When sellIn < 6 should increase quality from 20 to 23 on updateQuality`, () => {
+            gildedRose.items[0].sellIn = 5;
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(23);
+        });
+
+        it(`When sellIn < 6 and quality is 49 should increase quality to 50 on updateQuality`, () => {
+            gildedRose.items[0].sellIn = 5;
+            gildedRose.items[0].quality = 49;
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(50);
+        });
+
+        it(`When sellIn < 11 should increase quality from 20 to 22 on updateQuality`, () => {
+            gildedRose.items[0].sellIn = 10;
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(22);
+        });
+
+        it(`When SellIn < 0 should decrease quality to 0 on updateQuality`, () => {
+            gildedRose.items[0].sellIn = -1;
+            gildedRose.updateQuality();
+            expect(gildedRose.items[0].quality).to.equal(0);
         });
     });
 });
